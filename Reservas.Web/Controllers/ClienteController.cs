6c224a6a9323ae0af9e5fd14b7e043a4782e942b
Andres Hurtado <andreshw@gmail.com>
2015-02-23 11:53:06 -0500

    clase 01 mvc
    Se adiciona el avance de la clase del jueves 19 de febrero

---
+++ Reservas.Web/Controllers/ClienteController.cs
@@ -0,0 +1,38 @@
+﻿
+using Reservas.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Reservas.Web.Controllers
+{
+    public class ClienteController : Controller
+    {
+        //
+        // GET: /Cliente/
+        public ActionResult Index()
+        {
+            List<Cliente> clientes = Cliente.ObtenerClientes();
+            return View(clientes);
+        }
+
+        public ActionResult BuscarClientes(string nombre)
+        {
+            List<Cliente> clientes = Cliente.ObtenerClientes();
+
+            // si no se indica el nombre del cliente listarlos todos.
+            if (!string.IsNullOrEmpty(nombre))
+            {
+                clientes = clientes.Where(c => c.Nombre.StartsWith(nombre)).ToList();
+            }
+            return View(clientes);
+        }
+
+        public ActionResult CrearCliente()
+        {
+            return View();        
+        }
+	}
+}
---
+++ Reservas.Web/Models/Cliente.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Reservas.Web.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Ciudad { get; set; }
+        public int Calificacion { get; set; }
+
+        public static List<Cliente> ObtenerClientes()
+        {
+            return new List<Cliente>{
+                new Cliente{
+                    Id = 1,
+                    Nombre = "Katerine",
+                    Ciudad = "Apartadó",
+                    Calificacion = 8
+                },
+                new Cliente{
+                    Id = 2,
+                    Nombre = "Manuela",
+                    Ciudad = "Amalfi",
+                    Calificacion = 7
+                },
+                new Cliente{
+                    Id = 3,
+                    Nombre = "Laura",
+                    Ciudad = "Medellin",
+                    Calificacion = 9
+                },
+                new Cliente{
+                    Id = 4,
+                    Nombre = "Ana Maria",
+                    Ciudad = "Medellin",
+                    Calificacion = 8
+                }
+            };
+        }
+    }
+}
